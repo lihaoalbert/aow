acd306e9a575a36ea10c5acf825b188f875cf7bf
Camel Song <neosoyn@gmail.com>
2012-03-24 01:51:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aow::Application.config.secret_token = 'd975e9ab3d8db1d43a3480aabc03a8fe96770c2c000692e7f89c91ccd69b31148f531567354e3d954ee7b74ea6f807b518d9fca3e20125f9d83d2c6fce30ef4f'
