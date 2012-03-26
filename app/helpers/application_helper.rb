2938d4df0d4a139daeb05536bcbdb85e243aa5a7
Camel Song <neosoyn@gmail.com>
2012-03-26 21:24:36 +0800

    add layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Aow"
+    if page_title.empty?
+      base_title
+    else
+      %Q{#{base_title} | #{page_title}}
+    end
+  end
 end
