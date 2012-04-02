e421fb6b819e52fbe57d55ebc046b369d304dea1
Camel Song <neosoyn@gmail.com>
2012-04-03 00:03:45 +0800

    field_groups crud

--- app/models/field_group.rb
+++ app/models/field_group.rb
@@ -3,6 +3,11 @@ class FieldGroup < ActiveRecord::Base
   belongs_to :tag
   validates_presence_of :label
 
+  before_save do
+    #FIXME have bug when label is Chinese
+    self.name = label.downcase.gsub(/[^a-z0-9]/, '_') if name.blank? and label.present?
+  end
+
   def self.with_tags(tag_ids)
     where 'tag_id IS NULL OR tag_id IN (?)', tag_ids
   end
